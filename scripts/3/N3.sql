e148e8016f73973b66525edd41de065266836ec9
Simon <semyon_konovalov_sw@mail.ru>
2025-02-28 12:50:52 +0300

    Доработан 3й номер

--- scripts/3/N3.sql
+++ scripts/3/N3.sql
@@ -20,14 +20,15 @@ select d.second_name_teacher, d.title_discipline, s.n_group
 from discipline as d 
 join student_discipline as sd on sd.n_discipline = d.n_discipline
 join student as s on s.n_credit_book = sd.n_credit_book
-group by d.second_name_teacher, d.title_discipline, s.n_group;
+group by d.second_name_teacher, d.title_discipline, s.n_group
+order by d.second_name_teacher;
 
 -- 5.	Вывести список студентов из той же группы, что и Иванов.
-select name, second_name, n_group from student
+-- select name, second_name, n_group from student
 select second_name, n_group from student where n_group in (select distinct n_group from student where second_name = 'Иванов')
 
 -- 6.	Вывести список преподавателей, которые преподают тот же предмет, что и преподаватель Бурков.
-select second_name_teacher, n_discipline,title_discipline from discipline
+-- select second_name_teacher, n_discipline,title_discipline from discipline
 select second_name_teacher, title_discipline from discipline where title_discipline in (select distinct title_discipline from discipline where second_name_teacher = 'Смирнов')
 
 -- 7.	Вывести количество студентов, сдавших экзамены по всем предметам,
@@ -41,7 +42,15 @@ with good_student as (
 select count(n_credit_book) from good_student
 
 -- 8.	Вывести количество оценок 2, 3, 4, 5 полученных в каждой группе (использовать группировку).
-select estimation, count(estimation) from student_discipline group by estimation order by estimation
+SELECT 
+	s.n_group, 
+	e.estimation, 
+	COUNT(sd.estimation)
+FROM student as s
+CROSS JOIN (SELECT 2 AS estimation UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5) e
+LEFT JOIN student_discipline as sd ON s.n_credit_book = sd.n_credit_book AND sd.estimation = e.estimation
+GROUP BY s.n_group, e.estimation
+ORDER BY s.n_group, e.estimation
 
 -- 9.	Вывести средний балл оценок, полученных в каждой группе (использовать группировку).
 select s.n_group, avg(sd.estimation) from student_discipline as sd
@@ -49,9 +58,16 @@ join student as s on sd.n_credit_book = s.n_credit_book
 group by s.n_group order by s.n_group
 
 -- 10.	Вывести количество оценок 2, 3, 4, 5 по каждому предмету вместе с преподавателем, который принимал экзамен (использовать группировку).
-select d.title_discipline, sd.estimation, count(sd.estimation) from student_discipline as sd
-join discipline as d on sd.n_discipline = d.n_discipline
-group by d.title_discipline, sd.estimation order by d.title_discipline, sd.estimation
+SELECT 
+	d.title_discipline, 
+	e.estimation, 
+	COUNT(sd.estimation),
+	d.second_name_teacher
+FROM discipline d
+CROSS JOIN (SELECT 2 AS estimation UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5) e
+LEFT JOIN student_discipline sd ON d.n_discipline = sd.n_discipline AND sd.estimation = e.estimation
+GROUP BY d.title_discipline, e.estimation, d.second_name_teacher
+ORDER BY d.title_discipline, e.estimation 
 
 -- 11.	Вывести фамилии студентов, названия предметов, оценки, и фамилии преподавателей,
 -- которые принимали экзамен у данного студента по данному предмету (в виде таблицы (ФИО студента, Название предмета, Оценка, Фамилия преподавателя).
@@ -80,20 +96,20 @@ order by s.n_credit_book
 
 -- 14.	Узнать успеваемость студентки Беловой по всем предметам (написать 2 запроса: использующий естественное соединение и использующий подзапрос).
 select sd.n_credit_book, sd.estimation from student_discipline as sd
-join student as s on sd.n_credit_book = s.n_credit_book
+natural join student as s 
 where s.second_name = 'Иванов'
 
 select n_credit_book, estimation from student_discipline where n_credit_book in (select n_credit_book from student where second_name = 'Иванов')
 
 -- 15.	Вывести список групп, у которых принимал экзамен Бурков (написать 2 запроса: использующий естественное соединение и использующий подзапрос).
-select s.n_group 
-from student_discipline as sd
-join student as s on sd.n_credit_book = s.n_credit_book
-join discipline as d on sd.n_discipline = d.n_discipline
+select distinct s.n_group
+from student as s
+natural join student_discipline as sd
+natural join discipline as d
 where d.second_name_teacher = 'Сидорова'
 order by s.n_group
 
-select n_group from student 
+select distinct n_group from student 
 where n_credit_book in (
 	select n_credit_book from student_discipline
 	where n_discipline in (
@@ -103,14 +119,14 @@ where n_credit_book in (
 order by n_group
 
 -- 16.	Вывести список названий предметов, которые сдавала студентка Белова (написать 2 запроса: использующий естественное соединение и использующий подзапрос).
-select d.title_discipline 
+select distinct d.title_discipline 
 from student_discipline as sd
-join student as s on sd.n_credit_book = s.n_credit_book
-join discipline as d on sd.n_discipline = d.n_discipline
+natural join student as s 
+natural join discipline as d 
 where s.second_name = 'Иванов'
 order by d.title_discipline
 
-select title_discipline from discipline 
+select distinct title_discipline from discipline 
 where n_discipline in (
 	select n_discipline from student_discipline
 	where n_credit_book in (
@@ -134,8 +150,7 @@ select avg(sd.estimation) from student_discipline as sd
 join discipline as d on sd.n_discipline = d.n_discipline
 group by d.title_discipline
 having avg(sd.estimation) <= (select avg(sd.estimation) from student_discipline as sd
-join discipline as d on sd.n_discipline = d.n_discipline)
-
+								join discipline as d on sd.n_discipline = d.n_discipline)
 
 with avg_est as (
 	select avg(sd.estimation) as aest
@@ -145,7 +160,7 @@ with avg_est as (
 select avg(sd.estimation) from student_discipline as sd
 join discipline as d on sd.n_discipline = d.n_discipline
 group by d.title_discipline
-having avg(sd.estimation) <= (select min(aest) from avg_est)
+having avg(sd.estimation) <= (select avg(aest) from avg_est)
 
 
 
@@ -160,4 +175,3 @@ select s.n_group, avg(sd.estimation) from student_discipline as sd
 join student as s on sd.n_credit_book = s.n_credit_book
 group by s.n_group
 having avg(sd.estimation) >= (select avg(e) from tmp)
-
