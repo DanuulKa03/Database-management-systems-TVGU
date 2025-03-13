9bd6dc3097d08c752917f464bb56e44240210051
daniilkazakov <daniil.kazakov@piklema.com>
2025-03-13 22:42:51 +0300

    write task SQL

--- scripts/4/3.sql
+++ scripts/4/3.sql
@@ -1,3 +1,19 @@
-SELECT second_name FROM student as ST
-                   WHERE EXISTS(SELECT * FROM student_discipline as SD
-                                         WHERE ST.n_credit_book = SD.n_credit_book AND SD.estimation > 2 AND );
+-- 3. Вывести список студентов, сдавших больше, чем 1 экзамен,
+-- то есть получивших оценки выше «2» по двум или более предметам
+-- (2 способа: использовать вложенный запрос и группировку).
+SELECT n_credit_book, second_name, name FROM student AS ST WHERE
+    EXISTS ( SELECT 1 FROM student_discipline AS SD WHERE ST.n_credit_book = SD.n_credit_book AND estimation > 2
+    GROUP BY SD.n_credit_book HAVING COUNT(SD.n_discipline) > 1
+);
+
+-- Другой способ
+SELECT ST.n_credit_book, ST.second_name, ST.name
+FROM student AS ST
+         JOIN (
+            SELECT n_credit_book
+            FROM student_discipline
+            WHERE estimation > 2
+            GROUP BY n_credit_book
+            HAVING COUNT(n_discipline) > 1
+        ) AS PassedExams
+        ON ST.n_credit_book = PassedExams.n_credit_book;
